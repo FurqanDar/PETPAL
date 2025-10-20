4fe80004ce135366fd76d3f45ac5c9d311451bd1
noahg-neuroimage <noahg@wustl.edu>
2025-10-20 10:35:36 -0500

    docs: document class attributes

--- petpal/utils/stats.py
+++ petpal/utils/stats.py
@@ -4,7 +4,11 @@ import numpy as np
 import ants
 
 class RegionalStats:
-    """Run statistics on each region in a parametric 3D PET kinetic model or other image."""
+    """Run statistics on each region in a parametric 3D PET kinetic model or other image.
+    
+    :ivar pet_img: 3D PET image on which to get statistics for each region.
+    :ivar seg_img: Segmentation image in same space as `pet_img` defining regions.
+    :ivar stats_func: Callable function applied to voxels in PET image in each region."""
     def __init__(self,
                  input_image_path: str,
                  segmentation_image_path: str,
