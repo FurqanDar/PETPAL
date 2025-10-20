89731b946e32e14137f487458bbc649f3442254e
noahg-neuroimage <noahg@wustl.edu>
2025-10-20 10:32:50 -0500

    feat: new class to run regional statistics on 3D PET image

--- petpal/utils/stats.py
+++ petpal/utils/stats.py
@@ -1,1 +1,14 @@
 """Tools for running statistics on PET imaging data."""
+from collections.abc import Callable
+import numpy as np
+import ants
+
+class RegionalStats:
+    """Run statistics on each region in a parametric 3D PET kinetic model or other image."""
+    def __init__(self,
+                 input_image_path: str,
+                 segmentation_image_path: str,
+                 stats_func: Callable=np.mean):
+        self.pet_img = ants.image_read(input_image_path)
+        self.seg_img = ants.image_read(segmentation_image_path)
+        self.stats_func = stats_func
