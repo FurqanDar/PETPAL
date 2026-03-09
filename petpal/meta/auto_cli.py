f6c56c1ec248469cbd65719f0bce9353f6a663ae
noahg-neuroimage <noahg@wustl.edu>
2026-03-09 10:13:50 -0500

    feat: give variables appropriate names in auto cli

--- petpal/meta/auto_cli.py
+++ petpal/meta/auto_cli.py
@@ -38,49 +38,60 @@ def auto_cli(petpal_class: object):
     Args:
         petpal_class (object): Class defined in PETPAL that can be instantiated without specifying
             __init__ arguments. Must contain function __call__ with a docstring.
-    
 
-        Example:
+    Important:
+        The provided class must be designed such that any __init__ arguments can be set with
+        defaults, and those defaults should only be changed for testing and development purposes.
+        Otherwise, it would be impossible to tell which CLI arguments should be assigned to the
+        __init__ function and which should be assigned to __call__.
+
+        Additionally, any __call__ arguments and keyword arguments must be able to be specified from
+        the command line, such as strings, integers, or floats.
+
+    Example:
+        
+        .. code-block:: python
+
+            import numpy as np
+            from petpal.meta.auto_cli import auto_cli
+            import external_func
+
+            class MyClass:
+                mri_img: ants.ANTsImage
+                pet_img: ants.ANTsImage
+
+                def my_func(self, **kwargs):
+                    return external_func(self.mri_img, self.pet_img, **kwargs)
+
+                def __call__(self, mri_img_path, pet_img_path, out_img_path, **kwargs):
+                    self.mri_img = ants.image_read(mri_img_path)
+                    self.pet_img = ants.image_read(pet_img_path)
+                    output_img = my_func(**kwargs)
+                    ants.image_write(output_img, output_img)
             
-            .. code-block:: python
+                def main():
+                    # Creates CLI for class my_class
+                    # __call__ args are interpreted as required arguments
+                    # **kwargs is interpreted as optional keyword arguments
+                    # usage: petpal my-class --mri-img-path [MRI_IMG_PATH] \
+                    #          --pet-img-path [PET_IMG_PATH] --kwargs [kwarg1=val1 kwarg2=val2 ...]
 
-                import numpy as np
-                from petpal.meta.auto_cli import auto_cli
-                import external_func
+                    auto_cli(petpal_class=my_class)
 
-                class MyClass:
-                    mri_img: ants.ANTsImage
-                    pet_img: ants.ANTsImage
-
-                    def my_func(self, **kwargs):
-                        return external_func(self.mri_img, self.pet_img, **kwargs)
-
-                    def __call__(self, mri_img_path, pet_img_path, out_img_path, **kwargs):
-                        self.mri_img = ants.image_read(mri_img_path)
-                        self.pet_img = ants.image_read(pet_img_path)
-                        output_img = my_func(**kwargs)
-                        ants.image_write(output_img, output_img)
-                
-                    def main():
-                        # Creates CLI for class my_class
-                        # __call__ args are interpreted as required arguments
-                        # **kwargs is interpreted as optional keyword arguments
-                        # usage: --mri-img-path [MRI_IMG_PATH] --pet-img-path [PET_IMG_PATH] --kwargs [kwarg1=val1 kwarg2=val2 ...]
-
-                        auto_cli(petpal_class=my_class)
-
-                    if __name__=='__main__':
-                        main()
+                if __name__=='__main__':
+                    main()
                     """
     parser = argparse.ArgumentParser(prog=petpal_class.__name__,
                                      description=petpal_class.__call__.__doc__,
                                      formatter_class=argparse.RawTextHelpFormatter)
 
-    for _, v in inspect.signature(petpal_class.__call__).parameters.items():
-        name = str(v)
-        if name=='self':
+    call_signature = inspect.signature(petpal_class.__call__)
+    call_parameters = call_signature.parameters.items()
+    for _, call_parameter in call_parameters:
+        arg_name = str(call_parameter)
+        if arg_name=='self':
             continue
-        arg_and_type = name.split(': ')
+        arg_and_type = arg_name.split(': ')
         if len(arg_and_type)==2:
             arg_name = f'--{arg_and_type[0]}'.replace('_','-')
             arg_type = locate(arg_and_type[1])
